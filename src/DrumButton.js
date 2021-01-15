5f4c7a1361e73562645e23dd8a3627f3d536d573
Eugene Birondo <jbirondo@gmail.com>
2021-01-14 22:27:31 -0800

    Initial commit

---
+++ src/DrumButton.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function DrumButton(props){
+
+    useEffect(() => {
+        
+    })
+
+    return(
+        <button onClick={this.drumClick} >Hello World</button>
+    )
+}
+
+export default DrumButton
